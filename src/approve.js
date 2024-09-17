fb0a638213ddd8de8523011edfeaa065bfa38b5a
Jean Malaquias <jeancarlosm.goncalves@gmail.com>
2024-09-17 12:35:41 -0300

    criado novo arquivo para a função de aluno aprovado

---
+++ src/approve.js
@@ -0,0 +1,13 @@
+const nota = documnt.getElementById("media").value;
+
+function resultado(media){
+    let media = nota
+
+    if(media => 7){
+        return "Aprovado!"
+    }
+    else{
+        return "Reprovado!"
+    }
+}
+
---
+++ src/reportNameCres.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("name").value;
+const ra = document.getElementById("ra").value;
+const age = document.getElementById("age").value;
+
+
