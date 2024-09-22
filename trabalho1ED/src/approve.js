f1d73b6be223e3dc1e560e070c1d17e7f9554383
Jean Malaquias <jeancarlosm.goncalves@gmail.com>
2024-09-22 13:53:08 -0300

    adicionada função para aprovação aluno

--- trabalho1ED/src/approve.js
+++ trabalho1ED/src/approve.js
@@ -1,13 +1,28 @@
-const nota = documnt.getElementById("media").value;
 
-function resultado(media){
-    let media = nota
+const nota = document.getElementById("media");
+const resultado = document.getElementById("resultado");
 
-    if(media => 7){
-        return "Aprovado!"
+nota.addEventListener('blur', function() {
+    const media = parseFloat(nota.value); 
+
+
+    if (!isNaN(media)) {
+        let status
+        if (media >= 7) {
+            status = "Aprovado"
+            resultado.textContent = status
+            resultado.style.fontSize = "20px"; 
+        } else {
+            status = "Reprovado"
+            resultado.textContent = status
+            resultado.style.fontSize = "20px";
+        }
+
+        localStorage.setItem("StatusMedia", status)
+        localStorage.setItem("mediaValor", media)
+
+    } else {
+        resultado.textContent = ""; 
     }
-    else{
-        return "Reprovado!"
-    }
-}
+});
 
