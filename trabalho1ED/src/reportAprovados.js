98488302e4d9a00d4a189d6ce7a7afa7ffd5a61a
Jean Malaquias <jeancarlosm.goncalves@gmail.com>
2024-09-29 22:20:21 -0300

    criação relatório completo aprovados

---
+++ trabalho1ED/src/reportAprovados.js
@@ -0,0 +1,45 @@
+const report = document.getElementById("reportAprovados");
+let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+const gerarRelatorioBtn = document.getElementById("gerarRelatorioAprovados");
+
+function bubbleSort(arr) {
+    let trocou;
+    for (let i = 0; i < arr.length; i++) {
+        trocou = false;
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j].nome > arr[j + 1].nome) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                trocou = true;
+            }
+        }
+        if (!trocou) {
+            break;
+        }
+    }
+    return arr;
+}
+
+function exibirRelatorio() {
+    report.innerHTML = "";
+    
+    alunos = bubbleSort(alunos);
+    
+    const alunosAprovados = alunos.filter(aluno => aluno.status === "Aprovado");
+
+    alunosAprovados.forEach(aluno => {
+        let row = `
+            <tr>
+                <td>${aluno.nome}</td>
+                <td>${aluno.ra}</td>
+                <td>${aluno.idade}</td>
+                <td>${aluno.media}</td>
+                <td>${aluno.status}</td>
+            </tr>`;
+        
+        report.innerHTML += row;
+    });
+}
+
+gerarRelatorioBtn.addEventListener('click', exibirRelatorio);
